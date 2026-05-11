70606954c3ff8763812927a45d2b49fe8cbb3a1d
Jaimin Prajapati <jaimin4075@gmail.com>
2026-05-11 11:14:47 +0530

    fix admin dashboard auth

--- Router/Adminrouter.js
+++ Router/Adminrouter.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { AdminLogin } from "../Controller/AdminController.js";
+
+import {
+  AdminLogin,
+  AdminDashboard,
+} from "../Controller/AdminController.js";
+
+import { protect } from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// ✅ ADMIN LOGIN
 router.post("/login", AdminLogin);
+
+// ✅ ADMIN DASHBOARD
+router.get("/dashboard", protect, AdminDashboard);
 
 export default router;
